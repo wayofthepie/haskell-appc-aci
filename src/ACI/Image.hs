a6ac57f2eeeba8ae42b75d675b2e632c4236b051
wayofthepie <wayofthepie@gmail.com>
2015-11-30 22:40:27 +0000

    Add property test for ac id parser

--- src/ACI/Image.hs
+++ src/ACI/Image.hs
@@ -5,35 +5,47 @@
     , ScopedTypeVariables
     , TypeFamilies
     #-}
-module ACI.Image where
+module ACI.Image (
+    -- * ACI Image
+    createImage
+
+    -- ** Manifest
+    , ACIdentifier(..)
+    , acidParser
+    )where
 
 import qualified Codec.Archive.Tar as Tar
 import Control.Applicative
-import Data.Aeson
 import Data.Attoparsec.Text
 import qualified Data.Text as T
 import System.IO
 
 import Prelude hiding (takeWhile)
 
--- * ACI Image
-
--- | AC identifier type.
-newtype ACId = ACId T.Text
-
--- Parse an AC Identifier type, see
--- <https://github.com/appc/spec/blob/master/spec/types.md#ac-identifier-type>
-acidParser :: Parser ACId
-acidParser = ACId <$> ( initChar >>= parseAndBuild )
-  where
-    initChar = (T.pack . flip (:) []) <$> choice [letter, digit]
-    parseAndBuild c = T.append c <$>  (takeWhile $ inClass "a-z0-9./~")
-
-
+--------------------------------------------------------------------------------
+-- ACI Image
+--------------------------------------------------------------------------------
 
 -- | Create an image archive corresponding to the ACI specification.
 -- <https://github.com/appc/spec/blob/master/spec/aci.md#app-container-image ACI>
 createImage :: FilePath -> FilePath -> [FilePath] -> IO ()
 createImage = Tar.create
+
+
+--------------------------------------------------------------------------------
+-- Manifest
+--------------------------------------------------------------------------------
+
+-- | AC identifier type.
+newtype ACIdentifier = ACIdentifier T.Text deriving (Eq, Show)
+
+-- Parse an AC Identifier type, see
+-- <https://github.com/appc/spec/blob/master/spec/types.md#ac-identifier-type>
+acidParser :: Parser ACIdentifier
+acidParser = ACIdentifier <$> ( initChar >>= parseAndBuild )
+  where
+    initChar = (T.pack . flip (:) []) <$> choice [letter, digit]
+    parseAndBuild c = T.append c <$> (takeWhile $ inClass "-a-z0-9._/~")
+
 
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,16 +1,47 @@
+{-# LANGUAGE
+    OverloadedStrings
+    #-}
+import Data.Attoparsec.Text
+import qualified Data.Text as T
+import Test.Tasty
+import Test.Tasty.QuickCheck
 
 import ACI.Image
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain $ testGroup "ACI Image Property Tests"
+    [ manifestTestGroup
+    ]
+
+-- | Run a parser.
+run :: Parser a -> T.Text -> Maybe a
+run p input = case parseOnly p input of
+    Right x -> Just x
+    Left _  -> Nothing
 
 -------------------------------------------------------------------------------
--- tests for createImage
+-- Manifest parser property tests.
 -------------------------------------------------------------------------------
+manifestTestGroup :: TestTree
+manifestTestGroup = testGroup "manifest parser tests"
+   [ testProperty "valid acid spec" prop_ac_id_spec ]
 
--- <https://github.com/appc/spec/blob/master/spec/aci.md#image-manifest-schema>
-newtype AllowedManifest = AllowedManifest
-    {
-    } deriving (Eq,Show)
 
-instance
+data AllowedACId = AllowedACId T.Text ACIdentifier deriving (Eq, Show)
+
+instance Arbitrary AllowedACId where
+    arbitrary = genAcId >>= \txt -> pure $ AllowedACId txt (ACIdentifier txt)
+
+genAcIdInitChar :: Gen Char
+genAcIdInitChar = elements $ ['a'..'z'] ++ ['0'..'9']
+
+genAcId :: Gen T.Text
+genAcId = T.pack <$> ( genAcIdInitChar >>=
+    \c -> ((++) [c]) <$> elements ["-a-z0-9._/~"] )
+
+-- | Verifies the AC identifier parser follows the AC identifier
+-- specifications outlined here
+-- <https://github.com/appc/spec/blob/master/spec/types.md#ac-identifier-type>
+prop_ac_id_spec :: AllowedACId -> Bool
+prop_ac_id_spec (AllowedACId txt acid) = run acidParser txt == Just acid
+
