66e36ba42a6ada641d352373b310f5481cce09e7
Stephen O'Brien <wayofthepie@gmail.com>
2015-12-04 19:41:44 +0000

    Fix AC identifier parser
    Also reformats

--- src/ACI/Image.hs
+++ src/ACI/Image.hs
@@ -3,62 +3,65 @@
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies        #-}
+
 module ACI.Image where
 
 import qualified Codec.Archive.Tar    as Tar
 import           Control.Applicative
 import           Data.Attoparsec.Text
+import           Data.Char
+import           Data.Monoid
 import           Data.Proxy
 import qualified Data.Text            as T
+import           Debug.Trace
 import           GHC.TypeLits
-import           System.IO
-
 import           Prelude              hiding (takeWhile)
+import           System.IO
 
 --------------------------------------------------------------------------------
 -- ACI Image
---------------------------------------------------------------------------------
-
+-- -----------------------------------------------------------------------------
 -- | Create an image archive corresponding to the ACI specification.
 -- <https://github.com/appc/spec/blob/master/spec/aci.md#app-container-image ACI>
-createImage :: FilePath -> FilePath -> [FilePath] -> IO ()
+createImage
+    :: FilePath -> FilePath -> [FilePath] -> IO ()
 createImage = Tar.create
-
 
 --------------------------------------------------------------------------------
 -- Manifest
 --------------------------------------------------------------------------------
+newtype ACKind =
+    ACKind T.Text
+    deriving (Eq,Show)
 
--- | AC Kind Type. Currently there are only two types defined,
--- ImageManifest and PodManifest.
-{-
--- Some hacking with type level strings...
-
-newtype ACKindT (s :: Symbol) = ACKindT { acKindTValue :: T.Text } deriving (Eq, Show)
-
-consAcKind :: forall proxy s. KnownSymbol s => proxy s -> ACKindT s
-consAcKind _ = ACKindT (T.pack $ symbolVal (Proxy :: Proxy s))
-
-type ImageManifest = ACKindT "ImageManifest"
--}
-
-newtype ACKind = ACKind T.Text deriving (Eq, Show)
-
-acKinds :: [T.Text]
+-- | The kinds currently allowed.
+acKinds
+    :: [T.Text]
 acKinds = ["ImageManifest", "PodManifest"]
 
-acKindParser :: Parser ACKind
+-- | Parse a kind.
+acKindParser
+    :: Parser ACKind
 acKindParser = ACKind <$> choice (fmap string acKinds)
 
-
 -- | AC identifier type.
-newtype ACIdentifier = ACIdentifier T.Text deriving (Eq, Show)
+newtype ACIdentifier =
+    ACIdentifier T.Text
+    deriving (Eq,Show)
 
 -- Parse an AC Identifier type, see
 -- <https://github.com/appc/spec/blob/master/spec/types.md#ac-identifier-type>
-acidParser :: Parser ACIdentifier
-acidParser = ACIdentifier <$> ( initChar >>= parseAndBuild )
+acidParser
+    :: Parser ACIdentifier
+acidParser = toAcid <$> initAllowed <*> tailAllowed
   where
-    initChar = (T.pack . flip (:) []) <$> choice [letter, digit]
-    parseAndBuild c = T.append c <$> takeWhile (inClass "-a-z0-9._/~")
-
+    initAllowed = choice [letter, digit]
+    tailAllowed = do
+        t <- takeWhile (inClass "-a-z0-9._/~")
+        let endc = T.last t
+        if not (isLetter endc || isNumber endc)
+            then fail
+                     ("Expected to end with a character or a digit, found " ++
+                      [endc])
+            else return t
+    toAcid ic s = ACIdentifier $ T.cons ic s
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,67 +1,81 @@
 {-# LANGUAGE OverloadedStrings #-}
+
+import           ACI.Image
 import           Data.Attoparsec.Text
 import           Data.Maybe
 import           Data.Monoid
 import           Data.Proxy
 import qualified Data.Text                    as T
+import           Debug.Trace
 import           Test.Tasty
 import           Test.Tasty.HUnit
 import           Test.Tasty.Ingredients.Basic
 import           Test.Tasty.Options
 import           Test.Tasty.QuickCheck
 
-import           ACI.Image
-
-import           Debug.Trace
-
 main :: IO ()
-main = defaultMain . localOption (QuickCheckVerbose True) $
-    testGroup "ACI Image Property Tests"
-        [ manifestTestGroup
-        ]
-
+main =
+    defaultMain . localOption (QuickCheckVerbose True) $
+    testGroup "ACI Image Property Tests" [manifestTestGroup]
 
 -- | Run a parser.
-run :: Parser a -> T.Text -> Maybe a
-run p input = case parseOnly p input of
-    Right x -> Just x
-    Left _  -> Nothing
+run
+    :: Parser a -> T.Text -> Maybe a
+run p input =
+    case parseOnly p input of
+        Right x -> Just x
+        Left _ -> Nothing
 
--------------------------------------------------------------------------------
+-----------------------------------------------------------------------------
 -- Manifest parser property tests.
--------------------------------------------------------------------------------
-manifestTestGroup :: TestTree
-manifestTestGroup = testGroup "manifest parser tests"
-   [ testProperty "Valid App Container identifier spec" prop_ac_id_spec
-   , testCase "Valid App Container kind spec" test_ac_kind_spec
-   ]
+-----------------------------------------------------------------------------
+manifestTestGroup
+    :: TestTree
+manifestTestGroup =
+    testGroup
+        "manifest parser tests"
+        [ testProperty "Valid App Container identifier spec" prop_ac_id_spec
+        , testCase "Valid App Container kind spec" test_ac_kind_spec]
 
-
--------------------------------------------------------------------------------
--- | AC kind tests
-acKindStrings :: [T.Text]
+-----------------------------------------------------------------------------
+-- AC kind tests
+acKindStrings
+    :: [T.Text]
 acKindStrings = ["ImageManifest", "PodManifest"]
 
 test_ac_kind_spec :: IO ()
-test_ac_kind_spec = assert (isJust $ foldl1 (>>) $ fmap ( run acKindParser ) acKindStrings)
+test_ac_kind_spec =
+    assert (isJust $ foldl1 (>>) $ fmap (run acKindParser) acKindStrings)
 
--------------------------------------------------------------------------------
--- | AC identifier tests
-data AllowedACId = AllowedACId T.Text ACIdentifier deriving (Eq, Show)
+-----------------------------------------------------------------------------
+-- AC identifier tests
+data AllowedACId =
+    AllowedACId T.Text
+                ACIdentifier
+    deriving (Eq,Show)
 
 instance Arbitrary AllowedACId where
-    arbitrary = genAcId >>= \txt -> pure $ AllowedACId txt (ACIdentifier txt)
+    arbitrary =
+        genAcId >>=
+        \txt ->
+             pure $ AllowedACId txt (ACIdentifier txt)
 
-genAcIdInitChar :: Gen Char
-genAcIdInitChar = elements $ ['a'..'z'] ++ ['0'..'9']
+genAcIdInitChar :: Gen (Char, Char)
+genAcIdInitChar = do
+    c1 <- elements $ ['a' .. 'z'] ++ ['0' .. '9']
+    c2 <- elements $ ['a' .. 'z'] ++ ['0' .. '9']
+    return (c1, c2)
 
 genAcId :: Gen T.Text
-genAcId = T.pack <$> ( genAcIdInitChar >>=
-    \c -> (++) [c] <$> listOf1 (elements "-a-z0-9._/~"))
+genAcId = do
+    (initc,endc) <- genAcIdInitChar
+    surroundable <- T.pack <$> listOf1 (elements "-a-z0-9._/~")
+    return $ surround initc endc surroundable
+  where
+    surround ic ec str = T.cons ic $ T.snoc str ec
 
--- | Verifies the AC identifier parser follows the AC identifier
--- specifications outlined here
+-- | Verifies the AC identifier parser follows the AC identifier specifications outlined here
 -- <https://github.com/appc/spec/blob/master/spec/types.md#ac-identifier-type>
-prop_ac_id_spec :: AllowedACId -> Bool
+prop_ac_id_spec
+    :: AllowedACId -> Bool
 prop_ac_id_spec (AllowedACId txt acid) = run acidParser txt == Just acid
-
